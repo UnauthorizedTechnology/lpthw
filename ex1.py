8c0e202e8238c5d1e843744c997a92b7a9efd951
cutin4427 <cutin4426@gmail.com>
2019-03-26 21:46:36 -0700

    added exercises 1-3

---
+++ ex1.py
@@ -0,0 +1,8 @@
+print("Hello World!")
+print("Hello Again")
+print("I like typing like this.")
+print("This is fun.")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+
